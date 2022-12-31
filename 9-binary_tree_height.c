e5ea988eb8f44c36ff9bc6c8b3b160639e8d6854
Denatkins <dennisodibo@yahoo.com>
2022-12-31 08:19:58 -0800

    Binary Tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,9 +9,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-	int left = 0, right = 0;
+		int left = 0, right = 0;
 
-	if (tree->right)
+		if (tree->right)
 			right = 1 + binary_tree_height(tree->right);
 		if (tree->left)
 			left = 1 + binary_tree_height(tree->left);
